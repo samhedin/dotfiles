131177406687356b75577babd709c1961cef4afc
Sam Hedin <sam.hedin@gmail.com>
2020-08-23 03:44:32 +0200

    more binds

--- doom/.doom.d/keybinds.el
+++ doom/.doom.d/keybinds.el
@@ -2,46 +2,6 @@
 (defvar keybinds-mode-map (make-sparse-keymap)
   "Keymap for `my-mode'.")
 
-(map! :map keybinds-mode-map
-      :g "C-1" 'winum-select-window-1
-      :g "C-2" 'winum-select-window-2
-      :g "C-3" 'winum-select-window-3
-      :g "C-4" 'winum-select-window-4
-      :g "C-5" 'winum-select-window-5
-:g "C-<tab>" 'evil-switch-to-windows-last-buffer
-:g  "C-\"" 'recompile
-"C-!" 'kill-compilation
-      :i "C-¡" "::"
-      :i "C-@" "->"
-      :i "C-£" "<-"
-      :i "C-e" 'move-end-of-line
-      :i "C-n" 'forward-char
-      :g "C-." 'evil-avy-goto-char
-      :g "<f12>" 'eval-expression
-      :leader "!" 'shell-command
-      :leader "C-t" 'counsel-M-x
-      :g "C-l" 'switch-to-buffer
-      :g "C-_" '+popup/toggle)
-
-(map! :map evil-normal-state-map
-      "C-." nil
-      "C-<tab>" nil
-      "q" nil
-      )
-
-(map! :map undo-fu-mode-map "C-_" nil)
-
-(after! company
-  (map! :map company-active-map
-        "C-<return>" #'company-complete-selection
-        "<return>" nil
-        "RET" nil
-        "C-SPC" nil))
-
-(define-key keybinds-mode-map (kbd "C-SPC") 'evil-normal-state)
-(define-key evil-insert-state-map (kbd "C-SPC") nil)
-
-
 ;;;###autoload
 (define-minor-mode keybinds-mode
   "Keybinds"
@@ -57,10 +17,41 @@
 ;; `minor-mode-map-alist'
 (add-to-list 'emulation-mode-map-alists `((keybinds-mode . ,keybinds-mode-map)))
 
-;; Turn off the minor mode in the minibuffer
-(defun turn-off-my-mode ()
-  "Turn off my-mode."
-  (keybinds-mode -1))
-(add-hook 'minibuffer-setup-hook #'turn-off-my-mode)
+(map! :map keybinds-mode-map
+      :g "C-1" 'winum-select-window-1
+      :g "C-2" 'winum-select-window-2
+      :g "C-3" 'winum-select-window-3
+      :g "C-4" 'winum-select-window-4
+      :g "C-5" 'winum-select-window-5
+      :g "C-<tab>" 'evil-switch-to-windows-last-buffer
+      :g  "C-\"" 'recompile
+      "C-!" 'kill-compilation
+      :i "C-¡" "::"
+      :i "C-@" "->"
+      :i "C-£" "<-"
+      :i "C-e" 'move-end-of-line
+      :i "C-n" 'forward-char
+      :g "C-." 'evil-avy-goto-char
+      :g "<f12>" 'eval-expression
+      :leader "!" 'shell-command
+      :leader "C-t" 'counsel-M-x
+      :g "C-l" 'switch-to-buffer
+      :g "C-_" '+popup/toggle)
+
+(define-key evil-insert-state-map (kbd "C-SPC") 'evil-normal-state)
+(map! :map evil-normal-state-map
+      "C-." nil
+      "C-<tab>" nil
+      "q" nil)
+
+(map! :map undo-fu-mode-map "C-_" nil)
+(define-key undo-fu-mode-map (kbd "C-_") nil)
+
+(after! company
+  (map! :map company-active-map
+        "C-<return>" #'company-complete-selection
+        "<return>" nil
+        "RET" nil
+        "C-SPC" nil))
 
 (provide 'keybinds-mode)

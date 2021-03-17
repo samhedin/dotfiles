46ff97000567b0a4abd1b60fbaa628139b5883b4
Sam Hedin <sam.hedin@gmail.com>
2021-03-17 07:24:15 +0100

    working

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,40 +2,22 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, ... }:
 
-  nix = {
-    extraOptions = ''
-      cores = 8
-    '';
-    maxJobs = 16;
-  };
-  environment.variables.XDG_CURRENT_DESKTOP = "sway";
-  imports = [ # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
-  nixpkgs.config.allowUnfree = true;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
-  };
- services.logind.extraConfig = ''
-    RuntimeDirectorySize=50G
-    HandleLidSwitchDocked=ignore
-  '';
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+nixpkgs.config.allowUnfree = true;
+  boot.kernelPackages = pkgs.linuxPackages_5_10;
+  services.xserver.videoDrivers = ["amdgpu"];
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.useOSProber = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelModules = [ "v4l2loopback" ];
-  boot.extraModulePackages = [ pkgs.linuxPackages_latest.v4l2loopback ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback exclusive_caps=1 video_nr=9 card_label="OBS"
-  '';
 
-  networking.hostName = "nixos"; # Define your hostname.
+ networking.networkmanager.enable = true;
+ networking.hostName = "nixoslap"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
@@ -45,7 +27,7 @@
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
-  networking.interfaces.enp6s0.useDHCP = true;
+  networking.interfaces.wlp1s0.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -58,236 +40,69 @@
   #   keyMap = "us";
   # };
 
-  nix.autoOptimiseStore = true;
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome3.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
 
-  # services.xserver.displayManager.sddm.enable = true;
+
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
-  #  Configure keymap in X11
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "eurosign:e";
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
-  services.sshd.enable = true;
 
   # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+   sound.enable = true;
+   hardware.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.sam = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  };
-  nix.allowedUsers = [ "sam" ];
+   users.users.sam = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" "networkmanager"]; # Enable ‘sudo’ for the user.
+   };
 
-  nixpkgs.overlays = [
-    #omnisharp roslyn, remove when outdated.
-    (self: super: {
-      omnisharp-roslyn = super.omnisharp-roslyn.overrideAttrs (old: {
-        src = pkgs.fetchurl {
-          url =
-            "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.37.6/omnisharp-mono.tar.gz";
-          sha256 = "sha256-pebAU2s1ro+tq7AnaVKOIDoTjxM4dZwCRo1kJoARW+Y";
-        };
-      });
-    })
 
-  ];
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs;
-    let
-      machNix = import (builtins.fetchGit {
-        url = "https://github.com/DavHau/mach-nix/";
-        ref = "refs/tags/3.1.1";
-      }) { };
-      defaultPythonEnv = machNix.mkPython {
-        requirements = ''
-          pandas
-          numpy
-          tqdm
-          pygments
-          epc
-          pyqt5
-          PyQt5-sip
-          pygetwindow
-          qtconsole
-          jupyterlab
-          PyQtWebEngine
-          pymupdf
-          matplotlib
-          seaborn
-          scipy
-          black
-        '';
-        providers.cffi = "nixpkgs";
-      };
-    in [
-      stow
-      autoflake
-      xdotool
-      wmctrl
-      feh
-      swappy
-      aria
-      git
-      # tor-browser-bundle-bin
-      cask
-      qbittorrent
-      rustup
-      rust-analyzer
-      stack
-      htop
-      wget
-      appimage-run
-      nodejs
-      droidcam
-      mono6
-      dotnet-sdk_5
-      pdfgrep
-      julia
-      j4-dmenu-desktop
-      nodePackages.pyright
-      # https://jcodev.eu/posts/using-nix-for-haskell-development-in-emacs-with-lsp/
-      haskell-language-server
-      haskellPackages.cabal-install
-      haskellPackages.ghc
-      google-chrome
-      cabal2nix
-      electrum
-      ncdu
-      imagemagick
-      cmake
-      ninja
-      libsForQt5.qt5.qtwebengine
-      libsForQt5.qt5.qtbase
-      libsForQt5.qt5.qtx11extras
-      libsForQt5.qmake
-      nixfmt
-      vim
-      fd
-      ripgrep
-      firefox-wayland
-      ark
-      grim
-      slurp
-      unrar
-      libreoffice
-      alacritty
-      unityhub
-      unity3d
-      sqlite
-      omnisharp-roslyn
-      vscode
-      slack
-      defaultPythonEnv
-      machNix.mach-nix
-      xdg-desktop-portal-wlr
-      xdg-desktop-portal
-      pipewire
-      tldr
-      wf-recorder
-      qt5.qtwayland
-      qt5Full
-      gnumake
-      steam
-      nyxt
-      vlc
-      gcc
-      pandoc
-      zoom-us
-      spotify
-      discord-ptb
-      arc-theme
-      papirus-icon-theme
-      languagetool
-      pkgs.ntfsprogs
-      (pkgs.writeTextFile {
-        name = "startsway";
-        destination = "/bin/startsway";
-        executable = true;
-        text = ''
-          #! ${pkgs.bash}/bin/bash
-
-          # first import environment variables from the login manager
-          systemctl --user import-environment
-          # then start the service
-          exec systemctl --user start sway.service
-        '';
-      })
-    ];
-  systemd.user.targets.sway-session = {
-    description = "Sway compositor session";
-    documentation = [ "man:systemd.special(7)" ];
-    bindsTo = [ "graphical-session.target" ];
-    wants = [ "graphical-session-pre.target" ];
-    after = [ "graphical-session-pre.target" ];
-  };
-
-  systemd.user.services.sway = {
-    description = "Sway - Wayland window manager";
-    documentation = [ "man:sway(5)" ];
-    bindsTo = [ "graphical-session.target" ];
-    wants = [ "graphical-session-pre.target" ];
-    after = [ "graphical-session-pre.target" ];
-    # We explicitly unset PATH here, as we want it to be set by
-    # systemctl --user import-environment in startsway
-    environment.PATH = lib.mkForce null;
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''
-        ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
-      '';
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-  };
-
-  services.redshift = {
-    enable = true;
-    # Redshift with wayland support isn't present in nixos-19.09 atm. You have to cherry-pick the commit from https://github.com/NixOS/nixpkgs/pull/68285 to do that.
-    package = pkgs.redshift-wlr;
-  };
-
-  programs.waybar.enable = true;
-
-  systemd.user.services.kanshi = {
-    description = "Kanshi output autoconfig ";
-    wantedBy = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    serviceConfig = {
-      # kanshi doesn't have an option to specifiy config file yet, so it looks
-      # at .config/kanshi/config
-      ExecStart = ''
-        ${pkgs.kanshi}/bin/kanshi
-      '';
-      RestartSec = 5;
-      Restart = "always";
-    };
-  };
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    siji
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts
-    dina-font
-    proggyfonts
+   environment.systemPackages = with pkgs; [
+     wget vim
+     firefox
+     alacritty
+     wl-clipboard
+   ];
+   programs.light.enable = true;
+programs.sway = {
+  enable = true;
+  wrapperFeatures.gtk = true; # so that gtk works properly
+  extraPackages = with pkgs; [
+    brightnessctl
+    swaylock
+    swayidle
+    wl-clipboard
+    mako # notification daemon
+    alacritty # Alacritty is the default terminal in the config
+    dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
   ];
-
+};
+fonts.fonts = with pkgs; [
+  noto-fonts
+  noto-fonts-cjk
+  noto-fonts-emoji
+  liberation_ttf
+  fira-code
+  fira-code-symbols
+  mplus-outline-fonts
+  dina-font
+  proggyfonts
+];
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -295,27 +110,11 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true; # so that gtk works properly
-    extraPackages = with pkgs; [
-      swaylock
-      swayidle
-      wl-clipboard
-      mako # notification daemon
-      alacritty # Alacritty is the default terminal in the config
-      dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
-      kanshi
-      waybar
-      wdisplays
-    ];
-  };
-  location.latitude = 59.37118495540346;
-  location.longitude = 18.065956381997143;
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -329,7 +128,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
+  system.stateVersion = "21.05"; # Did you read the comment?
 
 }
-
